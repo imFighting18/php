c60dd374ea298d0803b63a2ab0d76d308443ecd5
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-25 12:11:03 -0300

    Create aula10.php

---
+++ aula10.php
@@ -0,0 +1,18 @@
+<?php
+
+$carros = array(1=>"BMW", 2=>"Ferrari", 3=>"Camaro");
+$carros[] = "Amarok";
+echo $carros[4]."<hr>";
+
+$motos = array();
+$motos = "Yamaha";
+$motos = "Honda";
+print_r($motos);
+echo "<hr>";
+
+$clientes = ["Rodrigo", "Felipe", "Gabriel"];
+echo count($clientes)."<hr>";
+
+foreach($clientes as $valor) {
+    echo $valor."<br>";
+}
