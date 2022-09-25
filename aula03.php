076f89c59cf2392b0bfbc5699fa73d963c18ebff
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-25 09:32:07 -0300

    Update aula03.php

---
+++ aula03.php
@@ -0,0 +1,13 @@
+<?php
+
+$nome = "Gabriel";
+$idade = 18;
+$altura = 1.65;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura! <br>";
+
+$nome = "Felipe";
+$idade = 30;
+$altura = 1.80;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura!";
