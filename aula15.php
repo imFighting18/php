2ca1a75839a6dc8f75deefc1a05775e398ff04cf
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-25 16:57:44 -0300

    Create aula15.php

---
+++ aula15.php
@@ -0,0 +1,21 @@
+<?php
+
+$cor = "verde";
+
+switch ($cor) {
+    case "vermelho":
+        echo "Cor é vermelho!";
+        break;
+    case "azul":
+        echo "Cor é azul!";
+        break;
+    case "amarelo":
+        echo "Cor é amarelo!";
+        break;
+    case "verde":
+        echo "Cor é verde!";
+        break;
+    default:
+        echo "Não existe essa cor!";
+        break;
+}
