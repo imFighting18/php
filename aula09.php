8f4af86540cef25f90450e0c0d5482c5c80585ff
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-25 12:07:56 -0300

    Create aula09.php

---
+++ aula09.php
@@ -0,0 +1,13 @@
+<?php
+
+$carros = array(1=>"BMW", 2=>"Ferrari", 3=>"Camaro");
+$carros[] = "Amarok";
+echo $carros[4]."<hr>";
+
+$motos = array();
+$motos = "Yamaha";
+$motos = "Honda";
+print_r($motos);
+
+$clientes = ["Rodrigo", "Felipe", "Gabriel"];
+print_r($clientes);
