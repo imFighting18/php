8fa476fbc7ac4363f2d10b47a68350d8a423f12a
imFighting18 <99926942+imFighting18@users.noreply.github.com>
2022-09-29 21:29:30 -0300

    Create aula21.php

---
+++ aula21.php
@@ -0,0 +1,17 @@
+<?php
+
+$contador = 1;
+
+while ($contador <= 10) {
+    echo $contador;
+    echo "<br>";
+    $contador++;
+}
+
+echo "<hr>";
+$contador = 1;
+do {
+    echo $contador;
+    echo "<br>";
+    $contador++;
+} while ($contador <= 10);
